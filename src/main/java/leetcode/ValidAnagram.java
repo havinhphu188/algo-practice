2978f50fafaab194f3f73c04a4aeef11b395d561
havinhphu188 <havinhphu188@gmail.com>
2020-12-15 21:52:23 +0700

    Enhance ValidAnagram

--- src/main/java/leetcode/ValidAnagram.java
+++ src/main/java/leetcode/ValidAnagram.java
@@ -1,15 +1,27 @@
 package leetcode;
+
 import java.util.Arrays;
 
 public class ValidAnagram {
 
+    //O(n) = max(length(s), length(t))
     public boolean isAnagram(String s, String t) {
-        char[] sortedS = s.toCharArray();
-        Arrays.sort(sortedS);
+        int[] counters = new int[26];
 
-        char[] sortedT = t.toCharArray();
-        Arrays.sort(sortedT);
+        for (int i = 0; i < s.length(); i++) {
+            counters[s.charAt(i)-'a'] +=1;
+        }
 
-        return Arrays.equals(sortedS,sortedT);
+        for (int i = 0; i < t.length(); i++) {
+            counters[t.charAt(i)-'a'] -=1;
+        }
+
+        for (int counter : counters) {
+            if (counter != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

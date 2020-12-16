a7db3dfad6241f3a14dd371c3ba44f6eecfc7840
havinhphu188 <havinhphu188@gmail.com>
2020-12-16 13:33:52 +0700

    optimize ProductOfArrayExceptSelf

--- src/main/java/leetcode/ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode/ProductOfArrayExceptSelf.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int length = nums.length;
-
-        int[] selfForward = new int[length];
-        int[] selfBackward = new int[length];
-        selfForward[length -1] = nums[length -1];
-        for (int i = length -2; i >= 0; i--) {
-            selfForward[i] = selfForward[i+1] * nums[i];
-        }
-        selfBackward[0] = nums[0];
-        for (int i = 1; i < length; i++) {
-            selfBackward[i] = selfBackward[i - 1] * nums[i];
-        }
+        int[] R = new int[length];
+        int[] L = new int[length];
         int[] result = new int[length];
-        for (int i = 1; i < length -1; i++) {
-            result[i]=selfBackward[i-1]*selfForward[i+1];
+
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = L[i - 1] * nums[i-1];
         }
-        result[0] = selfForward[1];
-        result[length -1] = selfBackward[length -2];
+
+        R[length-1] = 1;
+        for (int i = length-2; i >= 0 ; i--) {
+            R[i] = R[i + 1] * nums[i+1];
+        }
+
+        for (int i = 0; i < length; i++) {
+            result[i] = R[i] * L[i];
+        }
         return result;
     }
 }

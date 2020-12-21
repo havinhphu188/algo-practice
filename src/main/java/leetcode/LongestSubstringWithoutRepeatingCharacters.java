c32b76dd5eec0fdf907efe4cc2e904889d19dd86
havinhphu188 <havinhphu188@gmail.com>
2020-12-21 11:35:02 +0700

    LongestSubstringWithoutRepeatingCharacters convert to use set

--- src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,33 +1,25 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
-        Map<Character, Integer> count = new HashMap<>();
+        Set<Character> count = new HashSet<>();
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
 
-            if (count.get(c) == null){
-                count.put(c,0);
-            }
-
-            if (count.get(c) == 1) {
+            if (count.contains(c)) {
                 for( ; s.charAt(left) != c; left++){
-                    count.put(s.charAt(left),0);
+                    count.remove(s.charAt(left));
                 }
                 left++;
             }
             else{
-                count.put(c,1);
+                count.add(c);
             }
-
-            System.out.println("left = " + left);
-            System.out.println("right = " + right);
-            System.out.println(System.getProperty("line.separator"));
             maxLength = Math.max(maxLength, right - left + 1);
         }
         return maxLength;

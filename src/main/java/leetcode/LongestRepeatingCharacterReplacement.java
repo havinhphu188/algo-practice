13e57cf76bc6bc257988adfee5a2d55c67319d16
havinhphu <havinhphu188@gmail.com>
2021-04-18 23:14:37 +0700

    LongestRepeatingCharacterReplacement re implement

--- src/main/java/leetcode/LongestRepeatingCharacterReplacement.java
+++ src/main/java/leetcode/LongestRepeatingCharacterReplacement.java
@@ -3,12 +3,12 @@ package leetcode;
 //https://leetcode.com/problems/longest-repeating-character-replacement/discuss/91271/Java-12-lines-O(n)-sliding-window-solution-with-explanation/95833
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
-        int len = s.length();
         int[] count = new int[26];
-        int start = 0, maxCount = 0, maxLength = 0;
-        for (int end = 0; end < len; end++) {
-            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
-            while (end - start + 1 - maxCount > k) {
+        int maxCount = 0;
+        int start = 0; int end; int maxLength = 0;
+        for (end = 0; end < s.length(); end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end)-'A']);
+            while(end-start + 1 > k + maxCount){
                 count[s.charAt(start) - 'A']--;
                 start++;
             }

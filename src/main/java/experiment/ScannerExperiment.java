b2c7542dcce8b2627d17bbcc1fb93d5d36e1e7e6
havinhphu <havinhphu188@gmail.com>
2021-05-07 14:02:47 +0700

    add sample for ScannerExperiment

--- src/main/java/experiment/ScannerExperiment.java
+++ src/main/java/experiment/ScannerExperiment.java
@@ -5,6 +5,14 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+2
+3
+4 5 6
+fwefwef
+3 8 9
+*/
+
 public class ScannerExperiment {
 
     public static void main(String[] args) {

b6118e246e15b08f2ccd63585743db9103870b3f
havinhphu <havinhphu188@gmail.com>
2021-05-28 23:07:54 +0700

    Permutations

---
+++ src/main/java/leetcode/Permutations.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+//        System.out.println("nums = " + Arrays.toString(nums));
+        backTracking(nums, new ArrayList<>(), result);
+//        System.out.println("result = " + result);
+        return result;
+    }
+
+    public void backTracking(int[] nums, List<Integer> tempList, List<List<Integer>> result){
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+//            System.out.println("tempList = " + tempList);
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (tempList.contains(nums[i])) continue;
+            tempList.add(nums[i]);
+            backTracking(nums,tempList,result);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
---
+++ src/test/java/leetcode/PermutationsTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermutationsTest {
+    @Test
+    public void testCase1() {
+        Permutations sol = new Permutations();
+        sol.permute(new int[]{1,2,3});
+
+    }
+}

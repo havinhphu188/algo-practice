d45182bf1a1f466e83b4f81735e6e4139092ab15
havinhphu <havinhphu188@gmail.com>
2021-05-31 19:48:42 +0700

    NumOfGoodPair : Better solution

--- src/main/java/leedcodeeasy/NumOfGoodPair.java
+++ src/main/java/leedcodeeasy/NumOfGoodPair.java
@@ -3,18 +3,11 @@ package leedcodeeasy;
 import java.util.Arrays;
 
 public class NumOfGoodPair {
-    public int numIdenticalPairs(int[] nums) {
-//        System.out.println("nums = " + Arrays.toString(nums));
-        int pair = 0;
-        int[] map = new int[101];
-        for (int i = 0; i < nums.length; i++) {
-            map[nums[i]]++;
+    public int numIdenticalPairs(int[] A) {
+        int res = 0, count[] = new int[101];
+        for (int a: A) {
+            res += count[a]++;
         }
-
-        for (int i = 0; i < map.length ; i++) {
-            pair += map[i]*(map[i]-1)/2;
-        }
-
-        return pair;
+        return res;
     }
 }

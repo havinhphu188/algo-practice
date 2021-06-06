23f393bf05970532309cad53e04af2c0c5726f48
havinhphu <havinhphu188@gmail.com>
2021-06-06 23:39:46 +0700

    SubarraySumEqualsK Hashmap solution , recode

--- src/main/java/leetcode/SubarraySumEqualsK.java
+++ src/main/java/leetcode/SubarraySumEqualsK.java
@@ -5,16 +5,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SubarraySumEqualsK {
-    public int subarraySum(int[] nums, int k) {
-        int count = 0, sum = 0;
-        HashMap < Integer, Integer > map = new HashMap<>();
-        map.put(0, 1);
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k))
-                count += map.get(sum - k);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        public int subarraySum(int[] nums, int k) {
+    //        System.out.println("nums = " + Arrays.toString(nums));
+    //        System.out.println("k = " + k);
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int sum = 0;
+            int count = 0;
+            map.put(0, 1);
+            for (int num : nums) {
+                sum += num;
+                if (map.containsKey(sum - k)) {
+                    count += map.get(sum - k);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return count;
         }
-        return count;
-    }
 }

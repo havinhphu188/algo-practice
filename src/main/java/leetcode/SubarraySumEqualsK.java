91a975bfbf37a865a7b8e83ea31ad254d309819d
havinhphu <havinhphu188@gmail.com>
2021-06-06 23:08:26 +0700

    SubarraySumEqualsK Hashmap solution

--- src/main/java/leetcode/SubarraySumEqualsK.java
+++ src/main/java/leetcode/SubarraySumEqualsK.java
@@ -6,35 +6,15 @@ import java.util.Map;
 
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int totalSum = Arrays.stream(nums).sum();
-        int[] left = new int[nums.length];
-
-        int sumLeft = 0;
-        for (int i = 0, indexLeft = 1; i < nums.length - 1; i++, indexLeft++) {
-            sumLeft += nums[i];
-            left[indexLeft] = sumLeft;
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-
-        Map<Integer, Integer> rightMap = new HashMap<>();
-        rightMap.put(0, 1);
-        int[] right = new int[nums.length];
-        int sumRight = 0;
-        for (int i = nums.length - 1; i >= 1; i--) {
-            sumRight += nums[i];
-            right[i-1] = sumRight;
-            rightMap.put(sumRight, rightMap.getOrDefault(sumRight,0)+1);
-        }
-        int remainingSum = totalSum - k;
-        int result = 0;
-
-        for (int i = 0; i < left.length; i++) {
-            if (rightMap.containsKey(remainingSum - left[i])){
-                result += rightMap.get(remainingSum - left[i]);
-            }
-            rightMap.put(right[i], rightMap.get(right[i]) - 1);
-
-        }
-
-        return result;
+        return count;
     }
 }

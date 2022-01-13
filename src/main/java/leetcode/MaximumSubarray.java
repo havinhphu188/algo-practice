9f93efcbe53d964719926378b03da14b94e169c1
Ha Vinh Phu <havinhphu188@gmail.com>
2022-01-13 07:17:43 +0700

    space save implementation of Maximum Subarray

--- src/main/java/leetcode/MaximumSubarray.java
+++ src/main/java/leetcode/MaximumSubarray.java
@@ -1,22 +1,17 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        int currentSum = nums[0];
         int maxSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(
-                    nums[i],
-                    dp[i - 1] + nums[i] );
-            if (dp[i]>maxSum)
-                maxSum = dp[i];
+            if (currentSum < 0) {
+                currentSum = nums[i];
+            }else{
+                currentSum += nums[i];
+            }
+            maxSum = Math.max(currentSum, maxSum);
         }
-        System.out.println("nums = " + Arrays.toString(nums));
-        System.out.println("dp   = " + Arrays.toString(dp));
         return maxSum;
     }
 }

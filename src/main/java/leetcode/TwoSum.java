29579a03418593d1263458f5759f1749207d11c6
Ha Vinh Phu <havinhphu188@gmail.com>
2022-01-14 22:08:40 +0700

    Two sum better implementation

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -7,30 +7,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TwoSum {
-    Gson gson = new Gson();
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            List<Integer> listIndex = map.getOrDefault(nums[i], new ArrayList<>());
-            listIndex.add(i);
-            map.put(nums[i], listIndex);
+            if (map.containsKey(target - nums[i])) return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
         }
-
-        if (target % 2 == 0) {
-            if (map.containsKey(target/2)){
-                List<Integer> listIndex = map.get(target/2);
-                if (listIndex.size() >= 2) return new int[]{listIndex.get(0),listIndex.get(1)};
-            }
-        }
-
-        for (Integer key : map.keySet()) {
-            if (map.containsKey(target-key)){
-//                System.out.println("key = " + key);
-//                System.out.println("key - opposite = " + (target - key));
-                return new int[]{map.get(key).get(0), map.get(target - key).get(0)};
-            }
-        }
-        return new int[]{0,1};
+        return new int[]{};
     }
 }

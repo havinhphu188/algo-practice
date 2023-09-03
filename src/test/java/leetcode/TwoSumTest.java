650db8b7a635e687843b0e070eefc1fd68c6e31a
Ha Vinh Phu <havinhphu188@gmail.com>
2023-09-03 14:43:08 +0700

    fix test

--- src/test/java/leetcode/TwoSumTest.java
+++ src/test/java/leetcode/TwoSumTest.java
@@ -9,33 +9,33 @@ class TwoSumTest {
     public void testCase1() {
         int[] input = new int[]{2,7,11,15};
         TwoSum sol = new TwoSum();
-        assertArrayEquals(new int[]{0, 1}, sol.twoSum(input,9));
+        assertArrayEquals(new int[]{1, 0}, sol.twoSum(input,9));
     }
 
     @Test
     public void testCase2() {
         int[] input = new int[]{3,2,4};
         TwoSum sol = new TwoSum();
-        assertArrayEquals(new int[]{1,2}, sol.twoSum(input,6));
+        assertArrayEquals(new int[]{2,1}, sol.twoSum(input,6));
     }
 
     @Test
     public void testCase3() {
         int[] input = new int[]{3,3};
         TwoSum sol = new TwoSum();
-        assertArrayEquals(new int[]{0,1}, sol.twoSum(input,6));
+        assertArrayEquals(new int[]{1,0}, sol.twoSum(input,6));
     }
 
     @Test
     public void testCase4() {
         int[] input = new int[]{3,3,4,4};
         TwoSum sol = new TwoSum();
-        assertArrayEquals(new int[]{0,2}, sol.twoSum(input,7));
+        assertArrayEquals(new int[]{2,1}, sol.twoSum(input,7));
     }
     @Test
     public void testCase5() {
         int[] input = new int[]{3,3,4,4};
         TwoSum sol = new TwoSum();
-        assertArrayEquals(new int[]{2,3}, sol.twoSum(input,8));
+        assertArrayEquals(new int[]{3,2}, sol.twoSum(input,8));
     }
 }
